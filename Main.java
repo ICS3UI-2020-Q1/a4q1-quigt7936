d149fb10491031478c46c2a5653eab44e6db2858
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-02 14:40:40 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The program counts down from 100 by 5 to the given number
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // ask the user the number to count down from
+    System.out.println("Please enter an integer to count down to");
+    int userNumber = input.nextInt();
+
+    // delcare the number being said
+    int outputNumber = 100;
     
+    // while the number -5 is more than the said number continue
+    do {
+      System.out.println(outputNumber);
+      outputNumber = outputNumber - 5;
+    } while (outputNumber >= userNumber);
   }
 }
